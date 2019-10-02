c9a83469a2ac70c49a557e9ebe31ff582e0ec49c
McCarthey <mccarthey123@outlook.com>
2019-10-02 17:56:02 +0800

    [add] catdogs deploy

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # catdogs-fe
 Front-end using Next.js
+
+# deploy
+```
+pm2 start npm --name "next-catdogs" -- start
+```
---
+++ deploy.sh
@@ -0,0 +1,19 @@
+datanow=`date "+%Y-%m-%d %H:%M:%S"`
+commitStr='[deploy] auto deploy to catdogs at '$datanow  
+echo -e '\033[36m Wait for building\n \033[0m' && 
+{
+yarn build &&
+echo -e '\033[35m Build done, wait for pushing to git\n \033[0m' &&
+cp -r ./.next ../web-build/catdogs &&
+cp -r ./node_modules ../web-build/catdogs &&
+cp -r ./package.json ../web-build/catdogs &&
+cd ../web-build &&
+git pull &&
+git add . &&
+git commit -m "$commitStr" &&
+git push &&
+curl 'http://118.24.146.34:3001/'
+echo -e '\033[42;37m Deploy successfully!\033[0m You can refresh \033[32m http://118.24.146.34 \033[0m now to see your changes\n'
+} || {
+    echo -e '\033[41;37m Error\033[0m when building or git operation, check it manully please.'
+}

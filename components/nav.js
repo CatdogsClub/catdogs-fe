16bea99383c8a3e7bdb7654bb07b0d4754ee144d
mccarthey <mccarthey123@outlook.com>
2019-12-03 21:15:52 +0800

    [add] gif route

--- components/nav.js
+++ components/nav.js
@@ -2,8 +2,9 @@ import React from 'react'
 import Link from 'next/link'
 
 const links = [
+  { href: '/gif', label: 'GIF' },
   { href: 'https://zeit.co/now', label: 'ZEIT' },
-  { href: 'https://github.com/zeit/next.js', label: 'GitHub' }
+  { href: 'https://github.com/zeit/next.js', label: 'GitHub' },
 ].map(link => {
   link.key = `nav-link-${link.href}-${link.label}`
   return link
---
+++ pages/gif.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Nav from '../components/nav'
+
+const Gif = () => {
+    return [
+        <Nav />,
+        <div>this is gif</div>
+    ]
+}
+
+export default Gif

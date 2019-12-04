60458f8d76cef11cdf8c3832509dc6beeff838ad
mccarthey <mccarthey123@outlook.com>
2019-12-04 20:14:05 +0800

    [add] Tutorial page

---
+++ pages/tutorial.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Nav from '../components/nav'
+
+const Tutorial = () => {
+    return [
+        <Nav />,
+        <div>this is Tutorial</div>
+    ]
+}
+
+export default Tutorial 
